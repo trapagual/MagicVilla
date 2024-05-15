bda9255486cd7013003f5e7d774da43f19d57c6a
walt <trapagual@gmail.com>
2024-05-15 12:25:53 +0200

    Primeras lecciones hasta min 43:40

---
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_API.Modelos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa> {
+                new Villa(1, "Vista a la piscina"),
+                new Villa(2, "Vista a la playa")
+            };
+        }
+    }
+}
---
+++ MagicVilla_API/Modelos/Villa.cs
@@ -0,0 +1,14 @@
+﻿namespace MagicVilla_API.Modelos
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public Villa(int Id, string Nombre)
+        {
+            this.Id = Id;
+            this.Nombre = Nombre;
+        }
+    }
+}
